9af51257922af553a38c26262af928cf4087763a
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:05:24 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "The_Extraction_Point" `
+  -ModName "The Extraction Point" `
+  -ModFolder "EXP" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/the-extraction-point/EXP-Original-Cleaned.zip" `
+  -ModBaseFilesUrlHash "cfad3748d8520371f06b3a555958ebce3b870b0dc828113aa2408177f07e555a" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
